ccc5fd40095304117d114b340d70ed327b776110
Buse Ekmen <melissa.ekmen@gmail.com>
2023-08-08 11:12:33 -0400

    Update styles

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-console.log('hello world1');
-console.log('it\'s a good day'); 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
