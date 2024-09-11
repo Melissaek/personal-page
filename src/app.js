25ebfa3baca4ad8ee1bfaee055ec0b7069295818
Buse Ekmen <melissa.ekmen@gmail.com>
2024-09-11 13:38:45 -0400

    worked on wtr

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const slides = document.querySelector('.slides');
+const images = slides.querySelectorAll('img');
+const totalImages = images.length;
+let index = 0;
+let slideIndex = 0;
+showSlides();
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -11,3 +17,38 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     }))
+
+document.addEventListener('DOMContentLoaded', () => {
+    const goalItems = document.querySelectorAll('.goal-item');
+
+    goalItems.forEach(item => {
+        item.addEventListener('click', () => {
+            // Remove 'active' class from all goal items
+            goalItems.forEach(el => {
+                if (el !== item) { // Ensure that the clicked item is not affected
+                    el.classList.remove('active');
+                }
+            });
+
+            // Toggle 'active' class on the clicked goal item
+            item.classList.toggle('active');
+        });
+    });
+});
+
+
+function showSlides() {
+    const slides = document.querySelector('.slides');
+    const slidesArray = Array.from(slides.getElementsByClassName('slide'));
+    if (slideIndex >= slidesArray.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slidesArray.length - 1;
+    }
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
